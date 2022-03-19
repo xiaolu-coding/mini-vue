79a8f3a196770bf478fba8575eefd74925fafd22
xiaolu-coding <454819375@qq.com>
2022-03-19 12:29:02 +0800

    使用rollup打包

---
+++ rollup.config.js
@@ -0,0 +1,19 @@
+import pkg from './package.json'
+// import typescript from "@rollup/plugin-typescript"
+
+
+export default {
+  input: "./src/index.ts",
+  output: [
+    {
+      format: "cjs",
+      file: "lib/mini-vue.cjs.js",
+    },
+    {
+      format: "es",
+      file: "lib/mini-vue.esm.js",
+    },
+  ],
+
+  plugins: [typescript()],
+}
