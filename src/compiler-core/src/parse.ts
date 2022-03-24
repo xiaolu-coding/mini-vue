607d47ed5daf1836c0fe17fa3db2c4d85bddbe67
xiaolu-coding <454819375@qq.com>
2022-03-24 14:26:27 +0800

    feat: 解析text

--- src/compiler-core/src/parse.ts
+++ src/compiler-core/src/parse.ts
@@ -2,7 +2,7 @@ import { NodeTypes } from "./ast"
 
 const enum TagType {
   Start,
-  End
+  End,
 }
 
 export function baseParse(content: string) {
@@ -25,6 +25,10 @@ function parseChildren(context) {
       node = parseElement(context)
     }
   }
+  // 如果node没有值，就是text
+  if (!node) {
+    node = parseText(context)
+  }
   // 推进到nodes
   nodes.push(node)
   return nodes
@@ -43,11 +47,11 @@ function parseInterpolation(context) {
   // 减掉推进前的{{ 2 就是插值语法内部的文本长度
   const rawContentLength = closeIndex - openDelimiter.length
   // 截取出插值语法中的值 {{message}} -> message
-  const rawContent = context.source.slice(0, rawContentLength)
+  const rawContent = parseTextData(context, rawContentLength)
   // 去空格 {{ message }} 这种情况
   const content = rawContent.trim()
   // 截取}}后面的
-  advanceBy(context, rawContentLength + closeDelimiter.length)
+  advanceBy(context, closeDelimiter.length)
   // 返回相应的对象
   return {
     type: NodeTypes.INTERPOLATION,
@@ -79,7 +83,23 @@ function parseTag(context, type: TagType) {
     type: NodeTypes.ELEMENT,
     tag,
   }
-  
+}
+
+function parseText(context: any) {
+  const content = parseTextData(context, context.source.length)
+
+  return {
+    type: NodeTypes.TEXT,
+    content,
+  }
+}
+
+function parseTextData(context: any, length: number) {
+  // 1.获取content内容
+  const content = context.source.slice(0, length)
+  // 2.推进
+  advanceBy(context, content.length)
+  return content
 }
 // 推进 截取
 function advanceBy(context: any, length: number) {
