15f784bb76d98ba7f65a5a970bb7b73d44696974
xiaolu-coding <454819375@qq.com>
2022-03-24 17:08:17 +0800

    feat: transform

---
+++ src/compiler-core/src/transform.ts
@@ -0,0 +1,41 @@
+
+export function transform(root, options) {
+  // 全局上下文对象
+  const context = createTransformContext(root, options)
+  // 1、遍历 深度游侠搜索
+  traverseNode(root, context)
+  // 2、修改content
+}
+
+function createTransformContext(root: any, options: any) {
+  // 通过context存储外部传来的函数
+  const context = {
+    root,
+    nodeTransforms: options.nodeTransforms || []
+  }
+
+  return context
+}
+
+function traverseNode(node: any, context) {
+  // 从外部传来的函数
+  const nodeTransforms = context.nodeTransforms
+  // 获取到传入的函数，执行
+  for (let i = 0; i < nodeTransforms.length; i++) {
+    const transform = nodeTransforms[i]
+    transform(node)
+  }
+  traverseChildren(node, context)
+}
+
+function traverseChildren(node, context) {
+  const children = node.children
+  // 遍历树，深度优先搜索
+  if (children) {
+    for (let i = 0; i < children.length; i++) {
+      const node = children[i]
+
+      traverseNode(node, context)
+    }
+  }
+}
